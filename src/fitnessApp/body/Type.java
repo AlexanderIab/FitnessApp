cee2535687606cf1121177b60e69e4bf6adeb0c6
AlexanderIab <100590447+AlexanderIab@users.noreply.github.com>
2022-09-26 19:24:04 +0300

    Upd

--- src/fitnessApp/body/Type.java
+++ src/fitnessApp/body/Type.java
@@ -2,6 +2,9 @@ package fitnessApp.body;
 
 import java.time.LocalTime;
 
+/**
+ * @author Alexander Iablonski
+ * */
 public enum Type {
     SINGLE(new Zone[]{Zone.POOL, Zone.GYM}, LocalTime.of(8, 0), LocalTime.of(22, 0)),
     DAYTIME(new Zone[]{Zone.GYM, Zone.GROUP}, LocalTime.of(8, 0), LocalTime.of(16, 0)),
